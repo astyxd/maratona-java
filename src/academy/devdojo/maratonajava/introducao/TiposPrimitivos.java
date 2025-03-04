41d605209de1514cd914ca99d2edaedb07b229ae
Andre Gustavo <andgouveiap@gmail.com>
2025-03-04 15:59:18 -0300

    convenções de variáveis

---
+++ src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 23;
+        System.out.println("A idade e " + idade + " anos");
+    }
+}
