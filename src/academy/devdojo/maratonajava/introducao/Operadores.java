0a2b051e3f39b6638389fca42256e9b8efa52f2d
Andre Gustavo <andgouveiap@gmail.com>
2025-03-04 17:55:11 -0300

    operadores - logicos AND

--- src/academy/devdojo/maratonajava/introducao/Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Operadores.java
@@ -13,16 +13,23 @@ public class Operadores {
         System.out.println(resto);
 
         // < > <= >= == !=
-        boolean isDezMaiorQueVinte = 10 > 20;
-        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezMaiorQueVinte = 10 >= 20;
+        boolean isDezMenorQueVinte = 10 <= 20;
         boolean isDezIgualVinte = 10 == 20;
         boolean isDezIgualDez = 10 == 10.0;
         boolean isDezDiferenteDez = 10 != 10.0;
+        @@ -24,5 +24,15 @@ public static void main(String[] args) {
+            System.out.println("isDezIgualVinte "+isDezIgualVinte);
+            System.out.println("isDezIgualDez "+isDezIgualDez);
+            System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
 
-        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
-        System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
-        System.out.println("isDezIgualVinte "+isDezIgualVinte);
-        System.out.println("isDezIgualDez "+isDezIgualDez);
-        System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
+            // && (AND) || (or) !
+            int idade = 29;
+            float salario = 3500F;
+            boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+            boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+            System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
+            System.out.println("isDentroDaLeiMenorQueTrinta "+isDentroDaLeiMenorQueTrinta);
     }
 }
